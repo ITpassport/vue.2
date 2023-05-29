4c444130ccb8e2f5776aa58962bd16deee21fe45
ITpassport <tomato12wqq@gmail.com>
2023-05-29 14:45:14 +0900

    git commit

--- nuxt-project/nuxt.config.ts
+++ nuxt-project/nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
+export default { 
+components :[
+    { 
+    path: '@/components/'
+    pathPrefix :false
+}
+]
+}
 
-})
